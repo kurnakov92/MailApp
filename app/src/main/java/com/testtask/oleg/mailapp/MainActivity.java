35a2995cfae3e8767cc6f5cac2ca9e771944a0c3
Oleg <olegkurnakov@mail.ru>
2017-10-27 11:56:37 +0700

    Реализован функционал кнопки смены видимости символов пароля

--- app/src/main/java/com/testtask/oleg/mailapp/MainActivity.java
+++ app/src/main/java/com/testtask/oleg/mailapp/MainActivity.java
@@ -2,12 +2,31 @@ package com.testtask.oleg.mailapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton btnChangePassVisibility;
+    private EditText inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changePasswordVisibility(View view){
+        inputPassword = (EditText) findViewById(R.id.input_password);
+        btnChangePassVisibility = (ToggleButton) findViewById(R.id.btn_change_pass_visibility);
+        if (btnChangePassVisibility.isChecked()){
+            inputPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        } else {
+            inputPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+        }
     }
 }
